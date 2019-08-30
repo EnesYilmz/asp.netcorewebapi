5d27c2cc4882e2aef00bf758b2105d0c9747212d
Enes Yılmaz <39797616+EnesYilmz@users.noreply.github.com>
2019-08-30 22:24:31 +0300

    CRUD işlemleri için gerekli repository eklendi.

---
+++ asp.netcorewebapi/Data/IAppRepository.cs
@@ -0,0 +1,20 @@
+﻿using asp.netcorewebapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netcorewebapi.Data
+{
+    public interface IAppRepository
+    {
+        void Add<T>(T entity) where T:class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll();
+
+        List<City> GetCities();
+        City GetCityById(int cityId);
+        List<Photo> GetPhotosByCity(int cityId);
+        Photo GetPhoto(int id);
+    }
+}
