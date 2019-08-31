49fe550fa92a570e52f0c86c890715b450efaf00
Enes Yılmaz <39797616+EnesYilmz@users.noreply.github.com>
2019-08-31 11:29:31 +0300

    Dto katmanı ve automapper eklendi.
    Ayrıca veritabanı bağlantısı yapıldı.

---
+++ asp.netcorewebapi/Controllers/CitiesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using asp.netcorewebapi.Data;
+using asp.netcorewebapi.Dtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.netcorewebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _appRepository;
+        private IMapper _mapper;
+
+        public CitiesController(IAppRepository appRepository, IMapper mapper)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
+            return Ok(citiesToReturn);
+        }
+    }
+}
---
+++ asp.netcorewebapi/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+﻿using asp.netcorewebapi.Dtos;
+using asp.netcorewebapi.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.netcorewebapi.Helpers
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<City, CityForListDto>()
+                .ForMember(dest => dest.PhotoUrl,opt =>{
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                });
+        }
+    }
+}
